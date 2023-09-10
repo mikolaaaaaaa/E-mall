5b6fcc900ba68feeacf16a60d7f8c887073f0537
mikola++ <nick.velcom@gmail.com>
2023-09-10 17:31:34 +0300

    #E-mall [+] Eureka

--- client-service/src/main/java/by/mikola/client/ClientApplication.java
+++ client-service/src/main/java/by/mikola/client/ClientApplication.java
@@ -2,8 +2,10 @@ package by.mikola.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ClientApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/by/mikola/order/OrderApplication.java
+++ order-service/src/main/java/by/mikola/order/OrderApplication.java
@@ -2,14 +2,16 @@ package by.mikola.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class OrderServiceApplication {
+@EnableEurekaServer
+public class OrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
+        SpringApplication.run(OrderApplication.class, args);
     }
 
 }
