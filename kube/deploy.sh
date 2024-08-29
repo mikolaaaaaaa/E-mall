974bea3c7ebfbf960c12f96ac450ea846c9c9b6b
Nikolay Klimovich <n.klimovich@devtools.by>
2024-08-29 21:54:12 +0300

    E-mall [~] fix k8s deployment bags

--- kube/deploy.sh
+++ kube/deploy.sh
@@ -3,6 +3,8 @@
 # Start Minikube
 minikube start
 
+
+
 #upload all images
 minikube image load e-mall-db
 minikube image load mongo:latest
@@ -13,13 +15,9 @@ minikube image load confluentinc/cp-zookeeper:latest
 minikube image load confluentinc/cp-kafka:latest
 
 # Apply all configurations
-kubectl apply -f e-mall-db-service.yaml,mongo-service.yaml,zookeeper-service.yaml,kafka-service.yaml,eureka-server-service.yaml,order-service-service.yaml,client-service-service.yaml,e-mall-db-deployment.yaml,mongo-deployment.yaml,zookeeper-deployment.yaml,kafka-deployment.yaml,order-service-deployment.yaml,client-service-deployment.yaml,eureka-server-deployment.yaml
+kubectl apply -f namespace.yaml,e-mall-db-service.yaml,mongo-service.yaml,zookeeper-service.yaml,kafka-service.yaml,eureka-server-service.yaml,order-service-service.yaml,client-service-service.yaml,e-mall-db-deployment.yaml,mongo-deployment.yaml,zookeeper-deployment.yaml,kafka-deployment.yaml,order-service-deployment.yaml,client-service-deployment.yaml,eureka-server-deployment.yaml
 
-# Optional: Expose services to Minikube IP (if needed)
-#minikube service eureka-server --url
-#minikube service client-service --url
-#minikube service order-service --url
-
-minikube dashboard
-minikube service client-service
-minikube service order-service
+# Optional
+#minikube dashboard
+#minikube service client-service
+#minikube service order-service
